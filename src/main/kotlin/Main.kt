f94edfb6998e9a1b0dc14db3b078c469c52e1af9
hbouaraour <hbouaraour@gmail.com>
2024-07-14 10:30:05 +0200

    Changer le contexte

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 package fr.hamtec
 
 import fr.hamtec.creations.gest
+import fr.hamtec.creations.opDemar
 import fr.hamtec.creations.simpleRunBlocking
 
 fun main() {
-    println("Coroutine")
-    gest()
+    opDemar()
 }
--- src/main/kotlin/creations/CorDansCor.kt
+++ src/main/kotlin/creations/CorDansCor.kt
@@ -3,29 +3,56 @@ package fr.hamtec.creations
 import kotlinx.coroutines.*
 
 fun gest() {
+
     runBlocking {
 
-        val parentJob = GlobalScope.launch(CoroutineExceptionHandler { _, throwable ->
-            println("ERROR => ${throwable.message}")
-        }) {
+        val parentJob = GlobalScope.launch {
 
             println("Parent coroutine")
 
-            launch() {
+            launch {
                 println("First child")
                 delay(1000)
-                println("End of First child")
+                println("++ End of First child")
             }
 
-            launch {
+            launch(SupervisorJob()) {
                 println("Second child")
-                10 / 0
-                println("End of Second child")
+                1/0
+                println("++ End of Second child")
             }
+
             println("Fin parent coroutine")
         }
         parentJob.join()
     }
-
     println("Fin prog")
+}
+fun opDemar(): Unit {
+    runBlocking {
+
+        val parentJob = GlobalScope.launch {
+            println("Parent coroutine")
+
+            launch {
+                println("First coroutine")
+                delay(1000L)
+                println("End of first coroutine")
+            }
+
+            val childJob = async(start = CoroutineStart.LAZY) {
+                println("Second coroutine")
+                delay(1500L)
+                println("End of second coroutine")
+            }
+
+            childJob.await()
+
+        }
+
+        parentJob.join()
+        println("End of parentJob")
+    }
+
+    println("End of program")
 }
